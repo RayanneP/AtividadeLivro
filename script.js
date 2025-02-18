f9a7d75b8d42060cc6e4436bb98838be05882079
RayanneP <rayannepicinidasilva@gmail.com>
2025-02-18 15:33:42 -0300

    .

---
+++ script.js
@@ -0,0 +1,46 @@
+function selecionarEstrela(valor) {
+    document.getElementById("rating").value = valor;
+    let estrelas = document.querySelectorAll(".rating button img");
+    estrelas.forEach((estrela, index) => {
+        if (index < valor) {
+            estrela.src = "img/star1.png";
+        } else {
+            estrela.src = "img/star.png";
+        }
+    });
+}
+let lista = document.querySelectorAll(".item");  //Seleciona todos os elementos com a classe "item"
+//console.log(lista);
+
+let next = document.getElementById("proximo");  //Seleciona o botão de próximo
+let prev = document.getElementById("anterior");  //Seleciona o botão de anterior
+
+let contar = lista.length;  //Conta os elementos da lista
+let ativo = 0; // Índice do item ativo
+
+//console.log(contar)
+next.onclick=()=>{
+    console.log('proximo')
+    let desativar = document.querySelector('.ativo');
+    desativar.classList.remove('ativo');
+    if(ativo>=contar-1){
+        ativo=0;
+    }else{
+        ativo=ativo+1
+    }
+    lista[ativo].classList.add('ativo');
+}
+prev.onclick = () => {
+    console.log('anterior');
+    let desativar = document.querySelector('.ativo');
+    desativar.classList.remove('ativo');
+    if (ativo <= 0) {
+        ativo = contar - 1; // Volta para o último item se estiver no primeiro
+    } else {
+        ativo = ativo - 1;
+    }
+
+    lista[ativo].classList.add('ativo');
+};
+
+
